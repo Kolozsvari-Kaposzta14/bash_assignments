33311dad22fe7186f264f883dd87c8e2da1fc7f7
Kolozsvari-Kaposzta14 <tabara.liviu@gmail.com>
2026-09-01 17:11:10 +0300

    Enhance cleanup.sh with explanatory comments
    Added comments to explain the script's functionality.

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# For this script, I've used just one variable, to make it clear and concise. 
+# For this particular use case, the 'for' loop was the best choice. I've instructed the script
+# to look for dir in all directories, then it changed directories, located all files with the
+# .tmp extension, counted them, then it finally removed them all
+
 dir=/bash_assignments
 
 for dir in */

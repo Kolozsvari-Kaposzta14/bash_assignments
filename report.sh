28c1299b731764d8c776de870e2eb896845b5fd1
Kolozsvari-Kaposzta14 <tabara.liviu@gmail.com>
2026-09-01 17:21:00 +0300

    Update report.sh to log system information
    Add current directory, user, and last commands to system report

--- report.sh
+++ report.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Just as in my previous scripts, I've used the 'dir' variable to assign it the current 
+# working directory, so it could have been called later in the script. I've manually created
+# the system_report.txt file, then I've instructed the script to append it with the 
+# current working directory and the current user. The script was also instructed to display
+# the last 5 commands that have been used. 
+
 dir=/bash_assignments
 
 echo "$dir" >> system_report.txt && echo "$USER" >> system_report.txt

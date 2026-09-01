446d6e3236d85aee125e9b040bcb09270c7718c2
Kolozsvari-Kaposzta14 <tabara.liviu@gmail.com>
2026-09-01 17:13:30 +0300

    Refactor greeting to use variable for name
    Updated greeting to use variable 'name' instead of hardcoded value.

--- greet.sh
+++ greet.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# This script has been instructed to use 'Alice' as the main variable. The next 
+# command printed the name after being called by the dollar symbol, just as the 
+# date command has been called the same way. 
+
 name="Alice"
 
-echo "Hello Alice! Today is '$(date)'"
+echo "Hello '$name'! Today is '$(date)'"
 
 
